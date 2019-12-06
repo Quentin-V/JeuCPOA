cb9511149d418e455daafdbccd2f37d88180a742
Quentin <quentin.vauthier000@gmail.com>
2019-12-06 20:34:31 +0100

    Correction FrameErreur

--- src/FrameErreur.java
+++ src/FrameErreur.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 
 class FrameErreur extends JFrame {
 	FrameErreur(String s) {
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setTitle("Erreur");
+		this.setLocation(500, 500);
 		this.setLayout(new FlowLayout());
 		JLabel lblWarn = new JLabel();
 		try {
@@ -14,5 +17,7 @@ class FrameErreur extends JFrame {
 		}
 		this.add(lblWarn);
 		this.add(new JLabel(s));
+		this.pack();
+		this.setVisible(true);
 	}
 }
